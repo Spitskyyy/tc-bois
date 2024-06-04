0647b4d1f990cfc965151674cf5c51f9fe516145
Spitskyyy <grall.emeric@ik.me>
2024-06-04 14:50:01 +0200

    first commit

--- connexion.php
+++ connexion.php
@@ -1,30 +1,20 @@
 <?php
+//---------------------------------------------------------------------------------------------------------//
+// Connexion à la base de données MySQL
 session_start();
-
-use Dotenv\Dotenv;
 
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+// Charger les variables d'environnement à partir du fichier .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// Variable d'environnement
+// Récupérer les variables d'environnement
 $servername = $_ENV['BD_HOST'];
 $username = $_ENV['BD_USER'];
 $password = $_ENV['BD_PASS'];
 $dbname = $_ENV['BD_NAME'];
 
-
-$connection = mysqli_connect($servername, $username, $password, $dbname);
-
-if (mysqli_connect_error()) 
-{
-  echo 'Connexion echouer'. mysqli_connect_error();
-}
-else
-'Connexion reussie';
-
-// Connexion à la base de données
 try {
     $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -36,18 +26,25 @@ try {
 
         $_SESSION['email'] = $email;
 
-        // Vérifier si le compte existe
-        $stmt = $dbh->prepare("SELECT * FROM tbl_user WHERE mail_u = :email AND password_u = PASSWORD(CONCAT('*-6', :password))");
+        // Vérifier si le compte existe et si le mot de passe est correct
+        $stmt = $dbh->prepare("SELECT password_user FROM tbl_user WHERE mail_user = :email");
         $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':password', $password);
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
-            print(1); // Le compte existe
-            echo "Connexion réussie.";
-            header('location: /acceuil.php');
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($password, $row['password_user'])) {
+                // Mot de passe correct
+                print(1); // Le compte existe
+                echo "Connexion réussie.";
+                header('location: /acceuil.php');
+            } else {
+                // Mot de passe incorrect
+                echo "Nom d'utilisateur ou mot de passe incorrect.";
+                print(2);
+            }
         } else {
-            // Le compte n'existe pas ou les identifiants sont incorrects
+            // Compte non trouvé
             echo "Nom d'utilisateur ou mot de passe incorrect.";
             print(2);
         }
@@ -56,55 +53,99 @@ try {
     echo "Erreur : " . $e->getMessage();
     print(3);
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NDLP Avranche - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
-<body>
-    
-<!-- Outer Row -->
-<div class="row justify-content-center">
 
-<div class="col-xl-10 col-lg-12 col-md-9">
+<body class="bg-gradient-primary">
 
-    <div class="card o-hidden border-0 shadow-lg my-5">
-        <div class="card-body p-0">
-                    <div>
-                        <div>
-                            <h1>Bienvenue !</h1>
-                        </div>
+    <div class="container">
 
-                        <form class="user" method="POST" action="index.php">
-                            <div class="form-group">
-                                <input type="email" class="form-control form-control-user" name="email"
-                                    aria-describedby="emailHelp" placeholder="Enter Email Address">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block text-center">
+                                <!-- Ajout de la classe "text-center" pour aligner le contenu au centre -->
+                                <img src="../img/NDLP.png" width="90%"> <!-- Largeur de 75% de la colonne parente -->
                             </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control form-control-user"
-                                    name="password" placeholder="Enter mot de passe">
-                            </div>
-                            <br>
-                            <button type="submit" class="btn btn-primary btn-user btn-block">
-                                Connexion
-                            </button>
-                        </form>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Bienvenue !</h1>
+                                    </div>
 
-                        <hr>
-                        <div class="text-center">
-                            <a class="small" href="forgot-password.php">Mot de passe oublié?</a>
-                        </div>
-                        <div class="text-center">
-                            <a class="small" href="register.php">Créer un compte !</a>
+                                    <form class="user" method="POST" action="index.php">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user" name="email"
+                                                aria-describedby="emailHelp" placeholder="Enter Email Address">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                name="password" placeholder="Enter mot de passe">
+                                        </div>
+                                        <br>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Connexion
+                                        </button>
+                                    </form>
+
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.php">Mot de passe oublié?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.php">Créer un compte !</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </div>
+
+            </div>
+
         </div>
-    </div>  
-</div>   
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
 
 </body>
+
 </html>
