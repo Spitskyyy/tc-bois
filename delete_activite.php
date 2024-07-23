7028a84ca423d9eeeaa24c713b44f1461f2baea1
unknown <grall.emeric@ik.me>
2024-07-24 00:35:09 +0200

    FINI

--- delete_activite.php
+++ delete_activite.php
@@ -16,6 +16,49 @@ $dbname = $_ENV['BD_NAME'];
 // Connexion à la base de données
 $connection = new mysqli($servername, $username, $password, $dbname);
 
+
+// Récupération de l'email depuis la session
+$email = $_SESSION['email'];
+
+// Connexion à la base de données
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($connection->connect_error) {
+    die("La connexion a échoué : " . $connection->connect_error);
+}
+
+// Requête SQL pour obtenir le rôle de l'utilisateur
+$query = "SELECT tbl_role.name_r FROM tbl_role
+          JOIN tbl_user_role ON tbl_user_role.id_r_role = tbl_role.id_r
+          JOIN tbl_user ON tbl_user_role.id_user_user = tbl_user.id_user
+          WHERE tbl_user.mail_user = ?";
+$stmt = $connection->prepare($query);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$has_permission = false;
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        if ($row['name_r'] == 'PRO') {
+            $has_permission = true;
+            break;
+        }
+    }
+}
+
+if (!$has_permission) {
+    header("Location: index.php");
+    exit();
+}
+
+$stmt->close();
+
+
+
+
 // Vérifier la connexion
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
